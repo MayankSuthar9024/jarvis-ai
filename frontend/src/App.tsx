c4b37be8c6d5ab72bbfb5a9762575d6e212b0895
MayankSuthar9024 <memayanksuthar@gmail.com>
2026-08-13 18:36:01 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      {/* Empty container, showing only the background */}
+    </div>
+  )
+}
+
+export default App
